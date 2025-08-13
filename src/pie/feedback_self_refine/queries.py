d741aa5787aadcfd42fc9e918f008813e6e4a746
Orr Well <orrwella@gmail.com>
2025-08-14 00:01:06 +0300

    Updated prompts for pie task

--- src/pie/feedback_self_refine/queries.py
+++ src/pie/feedback_self_refine/queries.py
@@ -1,6 +1,6 @@
-# TODO: refine those
 FEEDBACK_INIT_Q="# Why is this code slow?"
-FEEDBACK_ON_FEEDBACK_Q="# What wrong with this feedback?"
+FEEDBACK_ON_FEEDBACK_Q="# What is wrong with this feedback?"
+# Another possible query: "# How can this feedback be improved?"
 ITERATE_Q="# Improved feedback:"
 # TODO: what should be the breaking sentence?
 PERFECT_FEEDBACK_WORDS="nothing wrong with this feedback"
