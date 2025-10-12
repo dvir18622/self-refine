7c7ac0300486107cbea00918092fab4c44229f1c
Orr Well <orrwella@gmail.com>
2025-10-12 13:07:07 +0300

    Typo fix + instructions for short feedback + perfect feedback example

--- src/pie/task_iterate.py
+++ src/pie/task_iterate.py
@@ -9,7 +9,7 @@ class PieIterate(Prompt):
     def __init__(self, engine: str, prompt_examples: str, temperature: float, feedback_type: str = "default") -> None:
         super().__init__(
             question_prefix="",
-            answer_prefix="# Improved version (only improved code, without any additional explnation):\n",
+            answer_prefix="# Improved version (only improved code, without any additional explanation):\n",
             intra_example_sep="\n\n",
             inter_example_sep="\n\n### END ###n\n",
         )
@@ -60,13 +60,13 @@ class PieIterate(Prompt):
 
 {feedback}
 
-# Improved version (only improved code, without any additional explnation):
+# Improved version (only improved code, without any additional explanation):
 
 """     
         
         query = example_template.format(slow_code=slow_code, feedback=feedback, instr=instr)
         if self.feedback_type == "none":
-            query = f"{slow_code}\n# Improved version (only improved code, without any additional explnation):\n"
+            query = f"{slow_code}\n# Improved version (only improved code, without any additional explanation):\n"
 
         return f"{self.prompt}{query}"
 
